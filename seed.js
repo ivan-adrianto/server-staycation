39eb5d06247d22edc93d32b5ea57151a2be2923f
Ivan Adrianto <ivanadrianto64@gmail.com>
2021-11-13 16:38:16 +0700

    Revert "setup mongo atlas"
    This reverts commit be1619005e393d4b759c83bae4573c9d29085c52.

--- seed.js
+++ seed.js
@@ -2,7 +2,7 @@ var seeder = require('mongoose-seed');
 var mongoose = require('mongoose');
 
 // Connect to MongoDB via Mongoose
-seeder.connect('mongodb+srv://ivanadrianto:ZvHt5MjNLQLXmINo@cluster0.gl7yr.mongodb.net/db-staycation?retryWrites=true&w=majority', {
+seeder.connect('mongodb://127.0.0.1:27017/db_staycation', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: true,
